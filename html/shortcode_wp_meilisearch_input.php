b9064b0a2acd7b3da9fcc338f085e9a9b06a56db
Sully Syed <ssyed@cfl.ca>
2020-08-08 08:54:58 -0400

    Results: Refine further to show facets, stats.

--- html/shortcode_wp_meilisearch_input.php
+++ html/shortcode_wp_meilisearch_input.php
@@ -10,29 +10,15 @@
 </div>
 
 <div class="rsp_section rsp_group">
-    <div class="rsp_col rsp_span_2_of_12">
-      <div id="clear-refinements"></div>
-
-        <h2>Content Types</h2>
+    <div id="wp_m_facets" class="rsp_col rsp_span_2_of_12">
+        <h3>Content Types</h3>
         <div id="content-types-list"></div>
 
-        <?php /*
-        <div id="wp_m_content_types_title">Content Types</div>
-        <ul id="wp_m_content_types_list">
-          <?php foreach ( $arr_post_types_selected as $arr_post_type ): ?>
-        <li>
-          <label for="content-type-option-<?php echo $arr_post_type; ?>" class="wp_m_content_type_option_label">
-              <input id="content-type-option-<?php echo $arr_post_type; ?>" type="checkbox" class="wp_m_content_type_option_checkbox" value="<?php echo $arr_post_types[$arr_post_type]->labels->singular_name; ?>" checked="checked" onclick="triggerSearch();">
-              <span class="wp_m_content_type_option_span"><?php echo $arr_post_types[$arr_post_type]->label; ?></span>
-            </label>
-        </li>
-          <?php endforeach; ?>
-        </ul>
-        */ ?>
+        <div id="clear-refinements"></div>
     </div>
 
-    <div class="rsp_col rsp_span_10_of_12">
-        <div id="wp_m_stats">&nbsp;</div>
+    <div id="wp_m_results" class="rsp_col rsp_span_10_of_12">
+        <div id="stats">&nbsp;</div>
 
         <div id="hits" style="margin: 0;">
             <!-- documents matching requests -->
@@ -61,6 +47,7 @@ function getMeiliSearchQueryParameter() {
     }
 };
 
+var bool_did_pageload_search = false;
 const search = instantsearch({
     indexName: "cfl",
     searchClient: instantMeiliSearch(
@@ -72,20 +59,18 @@ const search = instantsearch({
         }
     ),
     searchFunction( helper ) {
-        var str_query = getMeiliSearchQueryParameter();
-        if ( typeof str_query !== 'undefined' ) {
-            if ( str_query.length > 0 ) {
-                /*
-                var obj_searchbox = document.querySelector("[type='search']");
-                obj_searchbox.value = str_query;
-                */
+        if ( bool_did_pageload_search == false ) {
+            var str_query = getMeiliSearchQueryParameter();
+            if ( typeof str_query !== 'undefined' ) {
+                if ( str_query.length > 0 ) {
+                    helper.setQuery(str_query).search();
+                    bool_did_pageload_search = true;
 
-                helper.setQuery(str_query).search();
-
-                return;
+                    return;
+                }
             }
         }
-        
+
         helper.search();
     },
 });
@@ -113,32 +98,34 @@ search.addWidgets([
         container: "#content-types-list",
         attribute: "hierarchy_lvl1"
     }),
-
-  instantsearch.widgets.hits({
-    container: "#hits",
-    templates: {
-      item: `
-      <div class="wp_m_result">
-        <div class="">
-          <div class="wp_m_image">
-            {{#url_thumbnail}}
-              <a href="{{ url }}"><img src="{{ url_thumbnail }}"></a>
-            {{/url_thumbnail}}
-            {{^url_thumbnail}}
-              <a href="{{ url }}"><img src="/wp-content/themes/cfl.ca/images/og-image-default.jpg?_t=201902271522"></a>
-            {{/url_thumbnail}}
+    instantsearch.widgets.hits({
+        container: "#hits",
+        templates: {
+        item: `
+          <div class="wp_m_result">
+            <div class="">
+              <div class="wp_m_image">
+                {{#url_thumbnail}}
+                  <a href="{{ url }}"><img src="{{ url_thumbnail }}"></a>
+                {{/url_thumbnail}}
+                {{^url_thumbnail}}
+                  <a href="{{ url }}"><img src="/wp-content/themes/cfl.ca/images/og-image-default.jpg?_t=201902271522"></a>
+                {{/url_thumbnail}}
+              </div>
+              <div class="wp_m_metadata">{{ hierarchy_lvl1 }}&nbsp;&nbsp;|&nbsp;&nbsp;{{ date }}</div>
+              <div class="Xwp_m_title"><a href="{{ url }}">{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</a></div>
+              <div class="Xwp_m_content">{{#helpers.snippet}}{ "attribute": "content", "highlightedTagName": "mark" }{{/helpers.snippet}}</div>
+            </div>
           </div>
-          <div class="wp_m_metadata">{{ hierarchy_lvl1 }}&nbsp;&nbsp;|&nbsp;&nbsp;{{ date }}</div>
-          <div class="Xwp_m_title"><a href="{{ url }}">{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</a></div>
-          <div class="Xwp_m_content">{{#helpers.snippet}}{ "attribute": "content", "highlightedTagName": "mark" }{{/helpers.snippet}}</div>
-        </div>
-      </div>
-      `
-    }
-  }),
-  instantsearch.widgets.pagination({
-    container: "#pagination"
-  })
+          `
+        }
+    }),
+    instantsearch.widgets.pagination({
+        container: "#pagination"
+    }),
+    instantsearch.widgets.stats({
+        container: '#stats',
+    })
 ]);
 
 search.start();
